4687eb8ae003bbea337333811d99319bdeffd298
kcheung3102 <kcheung3102@gmail.com>
2021-10-28 21:35:03 -0400

    Updates

---
+++ precache-manifest.cea658d11e626b8c71faa5f39ff88525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6365b4eb18feef40561fc7b5cec374da",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "fc2fc4a118b187d0ac8f",
+    "url": "/pomodoro-clock/static/css/main.e1202080.chunk.css"
+  },
+  {
+    "revision": "9177d2013beb2b0f10ee",
+    "url": "/pomodoro-clock/static/js/2.f0481e1c.chunk.js"
+  },
+  {
+    "revision": "228bac5b7587832345b091357f4a52ce",
+    "url": "/pomodoro-clock/static/js/2.f0481e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2fc4a118b187d0ac8f",
+    "url": "/pomodoro-clock/static/js/main.80de141b.chunk.js"
+  },
+  {
+    "revision": "cfe2626f570e0945057f",
+    "url": "/pomodoro-clock/static/js/runtime-main.21255daa.js"
+  },
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/pomodoro-clock/static/media/watch-alarm.f3e1419d.mp3"
+  }
+]);
